12094c53041e1aa7cc42898263b78e55a09723b6
Shawn Nichol <7067shawnnichol@gmail.com>
2021-02-13 15:47:13 -0700

    Refactored file structure

--- app/src/main/java/com/example/androiddevpractice/room/dbInsert/FragmentsInsert.kt
+++ app/src/main/java/com/example/androiddevpractice/room/dbInsert/FragmentsInsert.kt
@@ -1,25 +1,26 @@
 package com.example.androiddevpractice.room.dbInsert
 
 import android.content.Context
+import com.example.androiddevpractice.room.Dev
 import com.example.androiddevpractice.room.DevDao
 
 suspend fun fragmentInsert(devDao: DevDao, mContext: Context) {
     // Fragments
-//                    var dev = Dev("Fragment", "Fragment", "Item 1 \nLine2")
-//                    devDao.insert(dev)
-//                    dev = Dev("LifeCycle", "Fragment", "")
-//                    devDao.insert(dev)
-//                    dev = Dev("Load Fragment", "Fragment", "")
-//                    devDao.insert(dev)
-//                    dev = Dev("Listenable Interface", "Fragment", "")
-//                    devDao.insert(dev)
-//                    dev = Dev("Flexible UI", "Fragment", "" )
-//                    devDao.insert(dev)
-//                    dev = Dev("Animation", "Fragment", "Animate & Transition" )
-//                    devDao.insert(dev)
-//                    dev = Dev("SharedElement", "Fragment", "Animate & Transition" )
-//                    devDao.insert(dev)
-//                    dev = Dev("Transitions Animation", "Fragment", "Animate & Transition" )
-//                    devDao.insert(dev)
+                    var dev = Dev("Fragment", "Fragment", "Item 1 \nLine2")
+                    devDao.insert(dev)
+                    dev = Dev("LifeCycle", "Fragment", "")
+                    devDao.insert(dev)
+                    dev = Dev("Load Fragment", "Fragment", "")
+                    devDao.insert(dev)
+                    dev = Dev("Listenable Interface", "Fragment", "")
+                    devDao.insert(dev)
+                    dev = Dev("Flexible UI", "Fragment", "" )
+                    devDao.insert(dev)
+                    dev = Dev("Animation", "Fragment", "Animate & Transition" )
+                    devDao.insert(dev)
+                    dev = Dev("SharedElement", "Fragment", "Animate & Transition" )
+                    devDao.insert(dev)
+                    dev = Dev("Transitions Animation", "Fragment", "Animate & Transition" )
+                    devDao.insert(dev)
 
 }
